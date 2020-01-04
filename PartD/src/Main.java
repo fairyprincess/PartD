01c4f428dfb887a2f85faf7709781d05d406173d
fairyprincess <gracefv@gmail.com>
2020-01-04 15:00:24 +1300

    Final

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	System.out.println("Hello, my name is Libby");
 	System.out.println("My favourite colour is pink (and silver!)");
 	System.out.println("My favourite mode of transportation is walking");
+	System.out.println("The best day of the week is any day I get to see my partner");
 }
 }

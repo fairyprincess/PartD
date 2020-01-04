a56670289a2590231f26c65ecb286df3c4e887c4
fairyprincess <gracefv@gmail.com>
2020-01-04 14:59:42 +1300

    Reread assignment for clarification

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
-	//Hello, my name is Libby
-	//My favourite colour is pink (and silver!)
-
+	public static void main(String[] args) {
+	System.out.println("Hello, my name is Libby");
+	System.out.println("My favourite colour is pink (and silver!)");
+	System.out.println("My favourite mode of transportation is walking");
+}
 }

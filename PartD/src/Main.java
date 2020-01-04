cb473c384199914ce4c3ab51e51235d937acd02c
fairyprincess <gracefv@gmail.com>
2020-01-04 15:05:11 +1300

    Added connectivity message.

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -7,3 +7,12 @@ public class Main {
 	System.out.println("The best day of the week is any day I get to see my partner");
 }
 }
+
+//Kia ora, I'm not sure where to document connection to Github so I'm putting it
+//in the comments of this small assignment. First, I created a repository in Github.
+//Then, I cloned the repository under the "Git Repositories" function in Eclipse. 
+//The repository didn't have a class or java file, so after creating a new java project
+//in eclipse, I merged the projects by utilising the team function in Eclipse and created
+//a new "main" class, where the code is now displayed. Finally, I wrote in the code and
+//saved it, sending it to GitHub as staged changes. These are documented in the commit
+//messages in the repository. Nga mihi.
